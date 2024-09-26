f1212a4906e4405d31d80a4d346833a31d7e1002
yura <yricog1290@gmail.com>
2024-09-26 17:14:10 +0900

    [Data Fetching]Server Side에서 data fetching하기

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,9 @@
-'use client';
-import { Metadata } from 'next';
-import { useEffect, useState } from 'react';
 /* 
-  route groups은 폴더 이름을 괄호로 묶는다 => (home)
+  route groups : 폴더 이름을 괄호로 묶는다 => (home)
   괄호로 묶었기 때문에 url은 생성되지 않는다
 */
+
+import { Metadata } from 'next';
 
 /**
  *  [ metadata ]
@@ -12,26 +11,37 @@ import { useEffect, useState } from 'react';
  *    - page 또는 layout 파일에서만 작성 가능
  *    - 각 페이지마다 다르게 설정하거나 중첩 가능
  * */
-// export const metadata: Metadata = {
-//   title: 'Home',
-// };
+export const metadata: Metadata = {
+  title: 'Home',
+};
 // --> 'use client' 컴포넌트에서는 사용 불가!
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      'https://nomad-movies.nomadcoders.workers.dev/movies',
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
-  useEffect(() => {
-    getMovies();
-  }, []);
+async function getMovies() {
+  // return fetch(URL).then((response) => response.json());
 
-  return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>;
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
 }
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
+}
+
+/**
+ * [ 기존 fetch 방식 ]
+ *  - 'use client'를 선언하므로 meta data를 사용할 수 없음
+ *  - client에서 React가 작동하기 때문에 항상 API를 써서 백엔드와 통신해야함
+ *    >> 그렇지 않으면 브라우저에서 아무나 DB 정보를 알 수 있기 때문에
+ *  - isLoading을 스스로 구현해야 함
+ */
+
+/**
+ * [ Server Side 방식 ]
+ *  - 브라우저 network에서 api를 확인할 수 없게됨!
+ *  - Next.js는 처음 fetch된 데이터를 기억한뒤, 이후에 API 요청을 하지 않는다
+ *  - API의 첫번째 요청에 대한 Loading 상태가 있을 수 있음
+ */
