ff5ed0e12884ea1e68d5cf6cc45557cd726dc483
yura <yricog1290@gmail.com>
2024-09-26 16:31:46 +0900

    [Data Fetching]client side에서 기존방식대로 data fetching하기

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,6 @@
+'use client';
 import { Metadata } from 'next';
+import { useEffect, useState } from 'react';
 /* 
   route groups은 폴더 이름을 괄호로 묶는다 => (home)
   괄호로 묶었기 때문에 url은 생성되지 않는다
@@ -10,14 +12,26 @@ import { Metadata } from 'next';
  *    - page 또는 layout 파일에서만 작성 가능
  *    - 각 페이지마다 다르게 설정하거나 중첩 가능
  * */
-export const metadata: Metadata = {
-  title: 'Home',
-};
+// export const metadata: Metadata = {
+//   title: 'Home',
+// };
+// --> 'use client' 컴포넌트에서는 사용 불가!
 
 export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies',
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>;
 }
